fe2a4dafa8899c6531e1c47baae38e8a23562b0d
APTOl301913 <APTOl301913@APOLBO03-56>
2023-03-30 17:42:10 +0530

    Add project files.

---
+++ APCRDATask/Controllers/APCRDAController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APCRDATask.Controllers
+{
+    public class APCRDAController : Controller
+    {
+        public ViewResult HOME()
+        {
+            return View();
+        }
+        public ViewResult ABOUTCRDA()
+        {
+            return View();
+        }
+    }
+}
